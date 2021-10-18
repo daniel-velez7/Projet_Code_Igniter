c5356af589adefdb3d24204e62dc7bdd59a4ce03
Nicolas-Plet <n.plet78@gmail.com>
2021-10-18 14:01:00 +0200

    Add model file

---
+++ CI_WebInfo/application/models/administrateur.php
@@ -0,0 +1,92 @@
+<?php
+
+class Administrateur extends CI_Model {
+
+    private $id;
+    private $nom;
+    private $prenom;
+    private $telephone;
+    private $email;
+    private $mdp;
+    private $adresse;
+
+    public function __construct($nom, $prenom, $telephone, $email, $mdp, $adresse) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->telephone = $telephone;
+        $this->email = $email;
+        $this->mdp = $mdp;
+        $this->adresse = $adresse;
+    }
+
+    public function set_id($newid) {
+        $this->id = $newid;
+    }
+
+    public function get_id() {
+        return $this->id;
+    }
+
+    public function set_nom($newNom) {
+        $this->nom = $newNom;
+    }
+
+    public function get_nom() {
+        return $this->nom;
+    }
+
+    public function set_prenom($newPrenom) {
+        $this->prenom = $newPrenom;
+    }
+
+    public function get_prenom() {
+        return $this->prenom;
+    }
+
+    public function set_telephone($newTelephone) {
+        $this->telephone = $newTelephone;
+    }
+
+    public function get_telephone() {
+        return $this->telephone;
+    }
+
+    public function set_email($newEmail) {
+        $this->email = $newEmail;
+    }
+
+    public function get_email() {
+        return $this->email;
+    }
+
+    public function set_mdp($newMdp) {
+        $this->mdp = $newMdp;
+    }
+
+    public function get_mdp() {
+        return $this->mdp;
+    }
+
+    public function set_adresse($newAdresse) {
+        $this->adresse = $newAdresse;
+    }
+
+    public function get_adresse() {
+        return $this->adresse;
+    }
+
+    //Fonction de simplification de l'utilisation de l'API PHP
+
+    public function Initialize($array) {
+        $this->id = $array['id'];
+        $this->name = $array['nom'];
+        $this->prenom = $array['prenom'];
+        $this->telephone = $array['telephone'];
+        $this->email = $array['email'];
+        $this->mdp = $array['mdp'];
+        $this->adresse = $array['adresse'];
+    }
+
+}
+
+?>
---
+++ CI_WebInfo/application/models/formateur.php
@@ -0,0 +1,123 @@
+<?php
+
+class Formateur {
+
+    id;
+    nom;
+    prenom;
+    cv;
+    photo;
+    email;
+    mdp;
+
+    constructor(nom, prenom, cv, photo, email, mdp) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.cv = cv;
+        this.photo = photo;
+        this.email = email;
+        this.mdp = mdp;
+    }
+
+    set_id(newid) {
+        this.id = newid;
+    }
+
+    get_id() {
+        return this.id;
+    }
+
+    set_nom(newNom) {
+        this.nom = newNom;
+    }
+
+    get_nom() {
+        return this.nom;
+    }
+
+    set_prenom(newPrenom) {
+        this.prenom = newPrenom;
+    }
+
+    get_prenom() {
+        return this.prenom;
+    }
+
+    set_cv(newCV) {
+        this.cv = newCV;
+    }
+
+    get_cv() {
+        return this.cv;
+    }
+
+    set_photo(newPhoto) {
+        this.photo = newPhoto;
+    }
+
+    get_photo() {
+        return this.photo;
+    }
+
+    set_email(newid) {
+        this.email = newEmail;
+    }
+
+    get_email() {
+        return this.email;
+    }
+
+    set_mdp(newMdp) {
+        this.mdp = newMdp;
+    }
+
+    get_mdp() {
+        return this.mdp;
+    }
+
+    //Fonction de simplification de l'utilisation de l'API PHP
+
+    Initialize(array) {
+        this.id = array['id'];
+        this.nom = array['nom'];
+        this.prenom = array['prenom'];
+        this.cv = array['cv'];
+        this.photo = array['photo'];
+        this.email = array['email'];
+        this.mdp = array['mdp'];
+    }
+
+    //Pour L'INSERT
+    GetArgumentsForInsert() {
+        return ' (nom, prenom, cv, photo, email, mdp) ';
+    }
+
+    GetValuesForInsert() {
+        return  " ('" + this.get_nom() + 
+                "', '" + this.get_prenom() + 
+                "', '" + this.get_cv() + 
+                "', '" + this.get_photo() + 
+                "', '" + this.get_email() + 
+                "', '" + this.get_mdp() + "')";
+    }
+
+    //Pour L'UPDATE
+    GetArgumentsForUpdate() {
+        return ['nom', 'prenom', 'cv', 'photo', 'email', 'mdp'];
+    }
+
+    GetValuesForUpdate() {
+        return [this.get_nom(), this.get_prenom(), this.get_cv(), this.get_photo(), this.get_email(), this.get_mdp()];
+    }
+
+    //Pour La  connection
+    GetArgumentsForSelectCondition() {
+        return ['email', 'mdp'];
+    }
+
+    GetValuesForSelectCondition() {
+        return [this.get_email(), this.get_mdp()];
+    }
+}
+
+?>
