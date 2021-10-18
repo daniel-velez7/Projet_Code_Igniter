dae9f3b05bfbd49da13ec57b9fa9bdf963354f33
Nicolas-Plet <n.plet78@gmail.com>
2021-10-18 11:02:27 +0200

    setup base info

--- CI_WebInfo/application/config/config.php
+++ CI_WebInfo/application/config/config.php
@@ -39,6 +39,26 @@ $config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
+| Auth File
+|--------------------------------------------------------------------------
+|
+| Typically this will be your index.php file, unless you've renamed it to
+| something else. If you are using mod_rewrite to remove the page set this
+| variable so that it is blank.
+|
+*/
+
+$config['auth'] = [
+    'user' => [
+        'table'     => 'users',
+        'login'     => 'email',
+        'password'  => 'password',
+        'encoding'  => 'bcrypt'
+    ]
+];
+
+/*
+|--------------------------------------------------------------------------
 | URI PROTOCOL
 |--------------------------------------------------------------------------
 |
